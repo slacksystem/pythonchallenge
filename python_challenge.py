24124f87b3945977eed66230a845dcb44b5bdf86
SlackSystem <slacksystem@users.noreply.github.com>
2022-08-31 12:09:46 -0400

    Add licence text and support for challenge 3
    Co-authored-by: Kat of SlackSystem

--- python_challenge.py
+++ python_challenge.py
@@ -1,4 +1,11 @@
+import urllib.request
 from pcutils import geturl
+from pregex.core.pre import Pregex
+from pregex.core.classes import AnyLetter, AnyWhitespace, AnyButLetter, AnyButFrom
+from pregex.core.groups import Capture, Group
+from pregex.core.quantifiers import OneOrMore, Optional, Indefinite
+from pregex.core.assertions import MatchAtEnd, NotFollowedBy
+from pregex.core.operators import Either
 
 
 def challenge1() -> str:
@@ -14,6 +21,22 @@ def challenge2(encodedMessage: str):
     return pagename
 
 
+def challenge3():
+    response = urllib.request.urlopen(geturl(challenge2("")))
+    siteBytes = response.read()
+    siteStr: str = str(siteBytes.decode("utf8"))
+    commentPre: Pregex = "<!--\n" + Capture(OneOrMore(Either(AnyButFrom(">", "-"), NotFollowedBy("-", "->"))), "comment") + "\n-->"  # type: ignore
+    comments = commentPre.get_matches(siteStr)
+    print(commentPre.get_captures(siteStr)[0])
+    blockOfMostlyNonsense = commentPre.get_captures(siteStr)[-1][0]
+    print(blockOfMostlyNonsense)
+    decipheredStr = "".join(AnyLetter().get_matches(blockOfMostlyNonsense))
+    # decipheredStr = "".join(pre.get_captures(siteStr))
+    # decipheredStr = siteStr
+    # return decipheredStr
+    return decipheredStr
+
+
 if __name__ == "__main__":
     print(geturl(challenge1()))
     print(
@@ -23,3 +46,4 @@ if __name__ == "__main__":
             )
         )
     )
+    print(geturl(challenge3()))
